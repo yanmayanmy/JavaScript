ed397dc796d8233e20bc2349535a2118d2d7718d
yanmayanmy <yuutayamauchi0109@gmail.com>
2022-03-02 08:52:23 -0600

    Completed Challenge 2 'Cat Generator'.

---
+++ Challenge2/static/js/script.js
@@ -0,0 +1,5 @@
+function generateCat (){
+    let catImg = document.createElement('img');
+    catImg.src = 'static/img/giphy.gif';
+    document.getElementById('flexbox-container').appendChild(catImg);
+}
