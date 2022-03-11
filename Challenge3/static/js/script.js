3b5ef3931b063ead384baf47a8ec9440fa89f0cd
yanmayanmy <yuutayamauchi0109@gmail.com>
2022-03-11 00:05:09 -0600

    Still working on challenge 3.

--- Challenge3/static/js/script.js
+++ Challenge3/static/js/script.js
@@ -1,7 +1,31 @@
 function rpsGame(yourChoice){
-    console.log(yourChoice);
-    console.log(yourChoice.id);
 
-    var humanHand, cpHand;
+    var humanHand, cpHand, final_result;
     humanHand = yourChoice.id;
+    cpHand = generateCpHand();
+    console.log(humanHand, cpHand);
+    final_result = judge(humanHand, cpHand);
+
+    console.log(final_result);
+
+}
+
+function generateCpHand(){
+
+    var randNum = Math.floor(Math.random() * 3);
+    return ['rock', 'paper', 'scissors'][randNum];
+}
+
+function judge(humanHand, cpHand){
+    var result;
+
+    if(humanHand == cpHand){
+        result = "Tie";    
+    }else if ((humanHand == "rock" && cpHand == "scissors") || (humanHand == "paper" && cpHand == "rock") || (humanHand == "scissors" && cpHand == "paper")) {
+        result = "You win";
+    }else {
+        result = "You lose";
+    }
+
+    return result;
 }
