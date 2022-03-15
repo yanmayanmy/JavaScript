8d3785d343600ea1cd6a4e874e3ee7cf6c77105a
yanmayanmy <yuutayamauchi0109@gmail.com>
2022-03-14 23:45:17 -0500

    Logic completed. Front-end remaining.

--- Challenge3/static/js/script.js
+++ Challenge3/static/js/script.js
@@ -2,30 +2,48 @@ function rpsGame(yourChoice){
 
     var humanHand, cpHand, final_result;
     humanHand = yourChoice.id;
-    cpHand = generateCpHand();
-    console.log(humanHand, cpHand);
+    cpHand = intToHand(generateRandInt());
     final_result = judge(humanHand, cpHand);
-
+    message = finalMessage(final_result)
+    console.log(humanHand);
+    console.log(cpHand);
     console.log(final_result);
+    console.log(message);
 
 }
 
-function generateCpHand(){
-
+function generateRandInt(){
     var randNum = Math.floor(Math.random() * 3);
-    return ['rock', 'paper', 'scissors'][randNum];
+    return randNum;
+}
+
+function intToHand(intNum) {
+    return ['rock', 'paper', 'scissors'][intNum];
+}
+
+function intToResult(intNum) {
+    return ['Winner', 'Draw', 'Loser'][intNum];
 }
 
 function judge(humanHand, cpHand){
-    var result;
-
-    if(humanHand == cpHand){
-        result = "Tie";    
-    }else if ((humanHand == "rock" && cpHand == "scissors") || (humanHand == "paper" && cpHand == "rock") || (humanHand == "scissors" && cpHand == "paper")) {
-        result = "You win";
-    }else {
-        result = "You lose";
+    rpsJudgement = {
+        "rock" : {"scissors" : 1, "rock" : 0.5, "paper" : 0},
+        "paper" : {"rock" : 1, "paper" : 0.5, "scissors" : 0},
+        "scissors" : {"paper" : 1, "scissors" : 0.5, "rock" : 0},
     }
+    
+    yourScore = rpsJudgement[humanHand][cpHand];
+    cpScore = rpsJudgement[cpHand][humanHand];
+    
+    return [yourScore, cpScore];
+}
 
-    return result;
+function finalMessage([yourScore, cpScore]){
+    if(yourScore === 1){
+        return {"message" : "You won!!", "color" : "blue"};
+    }else if(yourScore === 0.5){
+        return {"message" : "Draw", "color" : "yellow"};
+    }else{
+        return {"message" : "You lost...", "color" : "red"};
+    }
 }
