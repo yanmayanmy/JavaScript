765a625041ad47fe54559e75aa428e4956c92b6f
yanmayanmy <yuutayamauchi0109@gmail.com>
2022-03-24 10:15:54 -0500

    Finished making the prototype.

--- Challenge5/static/js/script.js
+++ Challenge5/static/js/script.js
@@ -1,8 +1,6 @@
-document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
-document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
-
-
-
+///////////////////
+// Game settings
+///////////////////
 let blackjackGame = {
     'you' : {'scoreSpan' : '#your-score', 'cardDiv' : '#your-card', 'score' : 0},
     'dealer' : {'scoreSpan' : '#dealer-score', 'cardDiv' : '#dealer-card', 'score' : 0},
@@ -10,10 +8,29 @@ let blackjackGame = {
     'cardsMap' : {'2' : 2, '3' : 3, '4' : 4, '5' : 5, '6' : 6, '7' : 7, '8' : 8, '9' : 9, '10' : 10, 'J' : 10, 'Q' : 10, 'K' : 10, 'A' : [1, 11]},
 }
 
+let scoreBox = {
+    'wins' : 0,
+    'losses' : 0,
+    'draws' : 0,
+}
+
+// Player Obj
 const YOU = blackjackGame['you'];
 const DEALER = blackjackGame['dealer'];
-const hitSound = new Audio('static/sounds/swish.m4a');
 
+// Sounds
+const hitSound = new Audio('static/sounds/swish.m4a');
+const winSound = new Audio('static/sounds/cash.m4a');
+const loseSound = new Audio('static/sounds/aww.m4a');
+
+// Adding events to the buttons
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+document.querySelector('#blackjack-stand-button').addEventListener('click', dealerLogic);
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
+
+///////////////////
+// Functions
+///////////////////
 function blackjackHit(){
     let card = randCard();
     showCard(card, YOU);
@@ -36,6 +53,8 @@ function blackjackDeal(){
     document.querySelector('#dealer-score').textContent = 0;
     document.querySelector('#your-score').style.color = 'rgb(211, 211, 211)';
     document.querySelector('#dealer-score').style.color = 'rgb(211, 211, 211)';
+    document.querySelector('#game-result').textContent = "Let's Play.";
+
 }
 
 function showCard(card, activePlayer){
@@ -73,4 +92,57 @@ function showScore(activePlayer){
     }else{
         document.querySelector(activePlayer['scoreSpan']).textContent = activePlayer['score'];
     }
+}
+
+function dealerLogic(){
+    while(DEALER['score'] < 17){
+        let card = randCard();
+        showCard(card, DEALER);
+        updateScore(card, DEALER);
+        showScore(DEALER);
+    }
+
+    showResult(computeWinner());
+}
+
+function computeWinner(){
+    let winner, resultMessage;
+
+    if(YOU['score'] <=  21){
+        if (DEALER['score'] > 21 || DEALER['score'] < YOU['score']) {
+            winner = YOU;
+        }else if(YOU['score'] < DEALER['score']){
+            winner = DEALER;
+        }else if(YOU['score'] === DEALER['score']){
+            winner = 'Draw'
+        }
+    }else{
+        winner = DEALER;
+    }
+
+    console.log(winner);
+
+    return winner;
+}
+
+function showResult(winner){
+    switch (winner) {
+        case YOU:
+            resultMessage = 'You won!';
+            scoreBox['wins'] += 1;
+            break;
+        case DEALER:
+            resultMessage = 'You loss...';
+            scoreBox['losses'] += 1;
+            break;
+        default:
+            resultMessage = 'Draw.';
+            scoreBox['draws'] += 1;
+            break;
+    }
+
+    document.querySelector('#score-box-wins').textContent = scoreBox['wins'];
+    document.querySelector('#score-box-losses').textContent = scoreBox['losses'];
+    document.querySelector('#score-box-draws').textContent = scoreBox['draws'];
+    document.querySelector('#game-result').textContent = resultMessage;
 }
